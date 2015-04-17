06ec0591532653b5ec69fa6f11c690863f8f8b9a
d1v3 <d1v3@outlook.com>
2015-04-17 00:48:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UberTrak_webApp_Main
---
+++ js/UberTrak.js
@@ -0,0 +1,26 @@
+var app = angular.module('single-page-app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partial/home.html'
+        })
+        .when('/about', {
+            templateUrl: 'partial/about.html'
+        })
+        .when('/stat', {
+            templateUrl: 'partial/stat.html'
+        })
+        .when('/profile', {
+            templateUrl: 'partial/profile.html'
+        });
+});
+
+
+app.controller('cfgController', function ($scope) {
+
+    /*      
+    Here you can handle controller for specific route as well.
+    */
+
+});
