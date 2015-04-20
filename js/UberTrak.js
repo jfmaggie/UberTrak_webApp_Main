a2967b5a10555452e57082bb38ee66497e96964a
lynntian <tianlingyu90@gmail.com>
2015-04-20 00:07:48 -0400

    add sections in stat page, WIP

--- js/UberTrak.js
+++ js/UberTrak.js
@@ -1,26 +1,46 @@
-var app = angular.module('single-page-app', ['ngRoute']);
+var app = angular.module('single-page-app', ['ui.router']);
 
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'partial/home.html'
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+    $stateProvider
+        .state('index', {
+            url: '/index'
+            //templateUrl: 'partial/home.html'
         })
-        .when('/about', {
+        .state('about', {
+            url: '/about',
             templateUrl: 'partial/about.html'
         })
-        .when('/stat', {
+        .state('stat', {
+            url: '/stat',
             templateUrl: 'partial/stat.html'
         })
-        .when('/profile', {
+        .state('profile', {
+            url: '/profile',
             templateUrl: 'partial/profile.html'
+        })
+        .state('stat.statDefault', {
+            url: '/statDefault',
+            templateUrl: 'partial/statDefault.html'
+        })
+        .state('stat.rideTime', {
+            url: '/rideTime',
+            templateUrl: 'partial/rideTime.html'
+        })
+        .state('home',{
+            url: '/home',
+            templateUrl: 'partial/home.html'
         });
-});
 
+
+});
 
 app.controller('cfgController', function ($scope) {
 
     /*      
     Here you can handle controller for specific route as well.
     */
+
+
 
 });
