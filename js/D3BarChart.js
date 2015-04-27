a65f623b41481cf50d53ec80c757831deeaf4931
lynntian <tianlingyu90@gmail.com>
2015-04-26 21:31:38 -0400

    Apr 26

---
+++ js/D3BarChart.js
@@ -0,0 +1,8 @@
+var data = [4, 8, 15,16,23,42];
+
+d3.select(".chart")
+	.selectAll("div")
+		.data(data)
+	.enter().append("div")
+		.style("width", function(d){return x(d) +"px";})
+		.text(function(d){ return d;});
