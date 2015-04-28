479a4181f3038b52286297e2919e04a9e3e5736d
QilongTang <173288704@qq.com>
2015-04-28 01:11:57 -0400

    Profile page layout using mockup data
    Work to be continued

---
+++ js/controller/RiderProfileController.js
@@ -0,0 +1,16 @@
+﻿//This is controller for profile partial page.
+
+app.controller('profileCtrl', function ($scope) {
+    console.log('PageProfile');
+
+    // Mockup data, to be replaced
+    $scope.userProfile =
+        {
+            "first_name": "Sherlock",
+            "last_name": "Holmes",
+            "email": "sherlock.Watson@uber.com",
+            "picture": "http://www.rlsandbox.com/img/profile.jpg",
+            "promo_code": "teypo",
+            "uuid": "91d81273-45c2-4b57-8124-d0165f8240c0"
+        }
+});
